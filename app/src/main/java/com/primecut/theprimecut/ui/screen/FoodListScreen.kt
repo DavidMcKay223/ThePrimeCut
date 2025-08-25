bf87bccbcc0d1eeb022b156256cfad1b23b08cd2
dmckay <dmckay@managewaresolutions.com>
2025-08-24 23:04:41 -0700

    DM - Fixing Lazy Load

--- app/src/main/java/com/primecut/theprimecut/ui/screen/FoodListScreen.kt
+++ app/src/main/java/com/primecut/theprimecut/ui/screen/FoodListScreen.kt
@@ -1,23 +1,23 @@
 package com.primecut.theprimecut.ui.screen
 
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.primecut.theprimecut.ui.component.FoodItemCard
 import com.primecut.theprimecut.ui.viewmodels.FoodItemViewModel
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.primecut.theprimecut.data.model.FoodItem
 
 @Composable
 fun FoodListScreen(
     viewModel: FoodItemViewModel = hiltViewModel()
 ) {
-    val foodItems = viewModel.foodItems
+    val foodItems: List<FoodItem> by viewModel.foodItems.collectAsState()
 
     LazyColumn {
-        items(foodItems.value) { item ->
+        items(foodItems) { item ->
             FoodItemCard(
                 foodItem = item
             )
