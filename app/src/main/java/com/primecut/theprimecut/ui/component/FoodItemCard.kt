52f8febe1d564c57e594884fd50fef5660df193a
dmckay <dmckay@managewaresolutions.com>
2025-08-24 22:58:17 -0700

    DM - Fixing Format

--- app/src/main/java/com/primecut/theprimecut/ui/component/FoodItemCard.kt
+++ app/src/main/java/com/primecut/theprimecut/ui/component/FoodItemCard.kt
@@ -3,14 +3,13 @@ package com.primecut.theprimecut.ui.component
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.primecut.theprimecut.data.model.FoodItem
 import com.primecut.theprimecut.ui.theme.DarkCharcoal
+import com.primecut.theprimecut.ui.theme.OffWhite
+import com.primecut.theprimecut.ui.theme.VividBlue
 
 @Composable
 fun FoodItemCard(
@@ -35,13 +34,45 @@ fun FoodItemCard(
                 Text(
                     text = foodItem.recipeName,
                     style = MaterialTheme.typography.titleMedium,
-                    color = Color.White
+                    color = VividBlue
                 )
-                Text(
-                    text = "${foodItem.caloriesPerServing} kcal",
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = Color.White
-                )
+                Row(modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+                    Text(
+                        text = "${foodItem.servings} Servings - (${foodItem.measurementServings} x ${foodItem.measurementType})",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = OffWhite
+                    )
+                    Text(
+                        text = "${foodItem.caloriesPerServing} cal",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = OffWhite
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween){
+                    Text(
+                        text = "Protein:\n${foodItem.protein} g",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = OffWhite
+                    )
+                    Text(
+                        text = "Carbs:\n${foodItem.carbs} g",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = OffWhite
+                    )
+                    Text(
+                        text = "Fats:\n${foodItem.fats} g",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = OffWhite
+                    )
+                    Text(
+                        text = "Fiber:\n${foodItem.fiber} g",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = OffWhite
+                    )
+                }
             }
         }
     }
